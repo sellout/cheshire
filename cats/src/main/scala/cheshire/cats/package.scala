1153317c63d09f1fe230ebc780225aec9c239a78
Aleksey Troitskiy <amtroitskiy@gmail.com>
2019-02-18 13:27:27 +0300

    category with products and arrow instances

--- cats/src/main/scala/cheshire/cats/package.scala
+++ cats/src/main/scala/cheshire/cats/package.scala
@@ -1,11 +1,16 @@
 package cheshire
 
-import scala.{Function1}
+import scala.{Function1, Tuple2}
+import _root_.cats.syntax.compose._
 
 import _root_.cats.{
   Applicative => CApplicative,
   Functor => CFunctor,
-  Traverse => CTraverse }
+  Traverse => CTraverse
+}
+
+import _root_.cats.arrow.{Arrow => CArrow}
+import _root_.cats.Id
 
 /** This package tries to make it easy to use existing Cats data types and type
   * classes with Cheshire.
@@ -23,5 +28,26 @@ package object cats {
     new Traverse[Function1, M, F] {
       def map[A, B](fn: Kleisli[Function1, M, A, B]) =
         Kleisli(F.traverse(_)(fn.runKleisli))
+    }
+
+  implicit def arrowFunctor[==>[_, _]: CArrow]: Functor[Function1, ==>, Id] = new Functor[Function1, ==>, Id] {
+    override def map[A, B](fn: A => B): A ==> B =
+      CArrow[==>].lift(fn)
+  }
+
+  implicit def arrowCategory[==>[_, _]](implicit A: CArrow[==>]): CategoryWithProducts[==>, Tuple2] =
+    new CategoryWithProducts[==>, Tuple2] {
+      override def product[A, B]: Product[==>, A, B, (A, B)] = new Product[==>, A, B, (A, B)] {
+        override def left: (A, B) ==> A = A.lift(_._1)
+        override def right: (A, B) ==> B = A.lift(_._2)
+        override def merge[X](l: X ==> A, r: X ==> B): X ==> (A, B) = A.merge(l, r)
+      }
+
+      override def identity[A]: A ==> A = A.id[A]
+
+      override def op = new FunctionB[ComposeB[==>, ==>, ?, ?], ==>] {
+        override def apply[A, B](fab: ComposeB[==>, ==>, A, B]): A ==> B =
+          fab.f <<< fab.g
+      }
     }
 }
---
+++ core/src/main/scala/cheshire/Category/Product.scala
@@ -0,0 +1,14 @@
+package cheshire
+
+import scala.{AnyKind}
+
+trait Product[⟶[_ <: AnyKind, _ <: AnyKind], A <: AnyKind, B <: AnyKind, `A × B` <: AnyKind] {
+  def left: ⟶[`A × B`, A]
+  def right: ⟶[`A × B`, B]
+
+  def merge[X](l: ⟶[X, A], r: ⟶[X, B]): ⟶[X, `A × B`]
+}
+
+trait CategoryWithProducts[⟶[_ <: AnyKind, _ <: AnyKind], ×[_ <: AnyKind, _ <: AnyKind]] extends Category[⟶] {
+  def product[A, B]: Product[⟶, A, B, A × B]
+}
