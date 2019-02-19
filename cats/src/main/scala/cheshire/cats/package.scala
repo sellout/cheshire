144dc04c1110ae0beff34c056b174d6accbefd02
Aleksey Troitskiy <amtroitskiy@gmail.com>
2019-02-19 12:29:33 +0300

    Arrow type

--- cats/src/main/scala/cheshire/cats/package.scala
+++ cats/src/main/scala/cheshire/cats/package.scala
@@ -1,6 +1,6 @@
 package cheshire
 
-import scala.{Function1, Tuple2}
+import scala.{Function1}
 
 import _root_.cats.{
   Applicative => CApplicative,
@@ -9,7 +9,6 @@ import _root_.cats.{
 
 import _root_.cats.arrow.{Arrow => CArrow}
 import _root_.cats.syntax.compose._
-import _root_.cats.Id
 
 /** This package tries to make it easy to use existing Cats data types and type
   * classes with Cheshire.
@@ -29,24 +28,14 @@ package object cats {
         Kleisli(F.traverse(_)(fn.runKleisli))
     }
 
-  implicit def arrowFunctor[==>[_, _]: CArrow]: Functor[Function1, ==>, Id] = new Functor[Function1, ==>, Id] {
-    override def map[A, B](fn: A => B): A ==> B =
-      CArrow[==>].lift(fn)
-  }
+  implicit def arrow[==>[_, _]](implicit A: CArrow[==>]): Arrow[==>] = new Arrow[==>] {
+    override def merge[A, B, X](l: X ==> A, r: X ==> B): X ==> (A, B) = A.merge(l, r)
 
-  implicit def arrowCategory[==>[_, _]](implicit A: CArrow[==>]): CategoryWithProducts[==>, Tuple2] =
-    new CategoryWithProducts[==>, Tuple2] {
-      override def product[A, B]: Product[==>, A, B, (A, B)] = new Product[==>, A, B, (A, B)] {
-        override def left: (A, B) ==> A = A.lift(_._1)
-        override def right: (A, B) ==> B = A.lift(_._2)
-        override def merge[X](l: X ==> A, r: X ==> B): X ==> (A, B) = A.merge(l, r)
-      }
-
-      override def identity[A]: A ==> A = A.id[A]
-
-      override def op = new FunctionB[ComposeB[==>, ==>, ?, ?], ==>] {
-        override def apply[A, B](fab: ComposeB[==>, ==>, A, B]): A ==> B =
-          fab.f <<< fab.g
-      }
+    override def op = new FunctionB[ComposeB[==>, ==>, ?, ?], ==>] {
+      override def apply[A, B](fab: ComposeB[==>, ==>, A, B]): A ==> B =
+        fab.f <<< fab.g
     }
+
+    override def map[A, B](fn: A => B): A ==> B = CArrow[==>].lift(fn)
+  }
 }
